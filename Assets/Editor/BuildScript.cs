47262987343150d07651a35540a103a2d573ebf7
f1081221002-prog <f1081221002@student.untan.ac.id>
2026-05-22 22:50:50 +0700

    Commit to main

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,26 +1,37 @@
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 
-public class BuildScript
+public class BuildScript : IPreprocessBuildWithReport
 {
-    // ANDROID
-    public static void SetAndroidIL2CPP()
-    {
-        PlayerSettings.SetScriptingBackend(
-            BuildTargetGroup.Android,
-            ScriptingImplementation.IL2CPP
-        );
+    public int callbackOrder => 0;
 
-        PlayerSettings.Android.targetArchitectures =
-            AndroidArchitecture.ARMv7 |
-            AndroidArchitecture.ARM64;
-    }
-
-    // WINDOWS
-    public static void SetWindowsIL2CPP()
+    public void OnPreprocessBuild(BuildReport report)
     {
-        PlayerSettings.SetScriptingBackend(
-            BuildTargetGroup.Standalone,
-            ScriptingImplementation.IL2CPP
-        );
+        // ANDROID
+        if (report.summary.platform == BuildTarget.Android)
+        {
+            PlayerSettings.SetScriptingBackend(
+                BuildTargetGroup.Android,
+                ScriptingImplementation.IL2CPP
+            );
+
+            PlayerSettings.Android.targetArchitectures =
+                AndroidArchitecture.ARMv7 |
+                AndroidArchitecture.ARM64;
+
+            UnityEngine.Debug.Log("ANDROID IL2CPP ENABLED");
+        }
+
+        // WINDOWS
+        if (report.summary.platform == BuildTarget.StandaloneWindows64)
+        {
+            PlayerSettings.SetScriptingBackend(
+                BuildTargetGroup.Standalone,
+                ScriptingImplementation.IL2CPP
+            );
+
+            UnityEngine.Debug.Log("WINDOWS IL2CPP ENABLED");
+        }
     }
 }
